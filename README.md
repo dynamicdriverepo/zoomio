d332174502d74e26fbaca8e543f7ebc22aa55a5f
George C <george077"gmail.com>
2017-03-18 14:41:15 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Zoomio- jQuery Image Zoom
+
+**Description:**  Zoomio is an easy to set up, **mobile friendly** Image Zoom script that overlays an enlarged image directly on top of the original image when activated. Users simply mouse over to bring up the enlarged image that can then be explored by moving the mouse around. On mobile devices, users tap the image to bring up the larger image that can then be dragged around.  The zoom function works by taking the original image and showing it in its native dimensions when triggered, meaning the same image is used as both the initial and "enlarged" image. Alternatively, you can specify a different, higher resolution image as the "enlarged" image that's loaded on demand instead.
+
+Zoomio can be called on the same image more than once to take into account any changes to the image, such as after updating the image's `src` property to zoom in on a different image.
+
+## Setup Information
+Zoomio is defined as a jQuery plugin. Simply call the `zoomio()` function on top of the desired image(s) to make them zoomable after the document has loaded:
+
+    jQuery(function($){ // on DOM load
+    	$(selector).zoomio()
+    })
+
+where selector is a valid jQuery selector selecting one or more images to invoke Zoomio on, such as:
+
+    $('#myimage').zoomio() // add Zoomio to a single image with ID "myimage"
+    $('.sampleimage').zoomio() // add Zoomio to all images with CSS class "sampleimage"
+
+For each image, it can consist of just a single image with **large native dimensions** that's scaled down initially using CSS when shown:
+
+    <img class="sampleimage" src="landscape.jpg" />
+
+Or a "thumbnail" image with a `data-largesrc` attribute defined that points to a larger, higher resolution version of itself. In this case the script will dynamically load the larger image on demand when the user mouses over the thumbnail:
+
+    <img class="sampleimage" src="landscapesmall.jpg data-largesrc="landscapelarge.jpg" />
+
+In general the second approach is better, as the large image is only loaded when the user actually zooms in on it. However, it does entail creating another version of each image.
+
+###Supported Options
+
+`$(selector).zoomio()` supports a small list of options you can enter to customize the zoom area interface for the target images:
+
+| Setting        | Description           |
+| ------------- |:-------------|
+| `fadeduration: milliseconds`      | The duration of the fade in effect, in milliseconds (ie: 1000 = 1 second). |
+| `w: widthString`      | The width of the zoom area interface in any desired unit, such as "300px", "80%" etc. By default Zoomio uses the width of the original image as the zoom area's width, creating an overlay that precisely overlaps the original image. |
+| `h: heightString` | The height of the zoom area interface in any desired unit, such as "300px", "80%" etc. By default Zoomio uses the height of the original image as the zoom area's height, creating an overlay that precisely overlaps the original image. |
+Example:
+
+    $('#myimage').zoomio({
+    	w: '80%',
+    	h: '300px' // <-- No comma after last option
+    })
+##Calling `zoomio()` more than once on the same image
+
+A cool feature of Zoomio is the ability to be called more than once on the same image, to take into account any changes made to the image. The most practical use of this is to update an image's src, then call Zoomio again on the image to make the new image zoomable as well:
+
+    <img id="celebrity" src="josiesmall.jpg" data-largesrc="josie.jpg">
+    
+    <script>
+    
+    $('#celebrity').zoomio() // enable Zoomio on this image
+    
+    function changeImage(imgid, thumbsrc, largesrc){
+    	document.getElementById(imgid).src = thumbsrc// update image src
+    	document.getElementById(imgid).setAttribute('data-largesrc', largesrc) //  update data-largesrc attribute with large image path
+    	$('#celebrity').zoomio() // reinitialize Zoomio on the image
+    }
+    
+    </script>
+    
+    <button onClick="changeImage('celebrity', 'millasmall.jpg', 'milla.jpg');">Milla</button> 
+
+Since we're making use of the `data-largesrc` attribute to specify a larger version of the original image, when updating it for another image, we need to update both the image's `src` and `data-largesrc` attributes. Otherwise, you should only be modifying the image's `src` property.
+
+Zoomio can also be used to create a simple image gallery where the showcased image can be zoomed in each time. See the **[project page](http://dynamicdrive.com/dynamicindex4/zoomio/)** for more an example of this.
---
+++ zoomio.js
@@ -0,0 +1,123 @@
+// Zoomio jQuery Image Zoom script
+// By Dynamic Drive: http://www.dynamicdrive.com
+// March 17th- Updated to v2.0, which now supports specifying a different, higher resolution image (via a data-largesrc attribute) to use as the zoomed in image.
+
+;(function($){
+	var defaults = {fadeduration:500}
+	var $zoomiocontainer, $zoomioloadingdiv
+	var currentzoominfo = { $zoomimage:null, offset:[,], settings:null, multiplier:[,] }
+	var ismobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) != null //boolean check for popular mobile browsers
+
+	function getDimensions($target){
+		return {w:$target.width(), h:$target.height()}
+	}
+
+	function getoffset(what, offsettype){ // custom get element offset from document (since jQuery version is whack in mobile browsers
+		return (what.offsetParent)? what[offsettype]+getoffset(what.offsetParent, offsettype) : what[offsettype]
+	}
+
+	function zoomio($img, settings){ // zoomio plugin function
+		var s = settings || defaults
+		var trigger = ismobile? 'touchstart' : 'mouseenter'
+		$img.off('touchstart mouseenter').on(trigger, function(e){ // on 'touchstart' or 'mouseenter'
+			var jqueryevt = e // remember jQuery event object (for use to call e.stopPropagation())
+			var e = jqueryevt.originalEvent.changedTouches? jqueryevt.originalEvent.changedTouches[0] : jqueryevt
+			var offset = {left:getoffset($img.get(0), 'offsetLeft'), top:getoffset($img.get(0), 'offsetTop') }
+			var mousecoord = [e.pageX - offset.left, e.pageY - offset.top]
+			var imgdimensions = getDimensions($img)
+			var containerwidth = s.w || imgdimensions.w
+			var containerheight = s.h || imgdimensions.h
+			$zoomiocontainer.stop().css({visibility: 'hidden'}) // hide loading DIV
+			var $zoomimage
+			var zoomdfd = $.Deferred()
+			var $targetimg = $img.attr('data-largesrc') || $img.data('largesrc') || $img.attr('src')
+			$targetimg += '?' + Math.random()
+			if ($img.data('largesrc')){
+				$zoomioloadingdiv.css({width:imgdimensions.w, height:imgdimensions.h, left:offset.left, top:offset.top, visibility:'visible', zIndex:10000}) // show loading DIV
+			}
+			$zoomiocontainer.html( '<img src="' + $targetimg + '"></div>' ) // add image inside zoom container
+			$zoomimage = $zoomiocontainer.find('img')
+			if ($zoomimage.get(0).complete){
+				zoomdfd.resolve()
+			}
+			else{
+				$zoomimage.on('load', function(){
+					zoomdfd.resolve()
+				})
+			}
+			zoomdfd.done(function(){
+				$zoomiocontainer.css({width:containerwidth, height:containerheight, left:offset.left, top:offset.top}) // set zoom container dimensions and position
+				var zoomiocontainerdimensions = getDimensions($zoomiocontainer)
+				var zoomimgdimensions = getDimensions($zoomimage)
+				$zoomioloadingdiv.css({zIndex: 9998})
+				$zoomiocontainer.stop().css({visibility:'visible', opacity:0}).animate({opacity:1}, s.fadeduration, function(){
+					$zoomioloadingdiv.css({visibility: 'hidden'})
+				}) // fade zoom container into view
+				if (ismobile){ // scroll to point where user tapped on
+					var scrollleftpos = (mousecoord[0] / imgdimensions.w) * (zoomimgdimensions.w - zoomiocontainerdimensions.w)
+					var scrolltoppos = (mousecoord[1] / imgdimensions.h) * (zoomimgdimensions.h - zoomiocontainerdimensions.h)
+					$zoomiocontainer.scrollLeft( scrollleftpos )
+					$zoomiocontainer.scrollTop( scrolltoppos )
+				}
+				currentzoominfo = {$zoomimage:$zoomimage, offset:offset, settings:s, multiplier:[zoomimgdimensions.w/zoomiocontainerdimensions.w, zoomimgdimensions.h/zoomiocontainerdimensions.h]}
+			})
+
+			$img.off('mouseleave').on('mouseleave', function(e){
+				if (zoomdfd.state() !='resolved'){ // if enlarged image has loaded yet when user mouses out of original image
+					zoomdfd.reject()
+					$zoomioloadingdiv.css({visibility: 'hidden'})
+				}
+			})
+			jqueryevt.stopPropagation() // stopPropagation() works on jquery evt object (versus jqueryevt.originalEvent.changedTouches[0]
+		})		
+	}
+
+	$.fn.zoomio = function(options){ // set up jquery zoomio plugin
+		var s = $.extend({}, defaults, options)
+
+		return this.each(function(){ //return jQuery obj
+			var $target = $(this)
+
+			$target = ($target.is('img'))? $target : $target.find('img:eq(0)')
+			if ($target.length == 0){
+				return true
+			}
+			zoomio($target, s)
+		}) // end return this.each
+
+	}
+
+	$(function(){ // on DOM load
+		$zoomiocontainer = $('<div id="zoomiocontainer">').appendTo(document.body)
+		$zoomioloadingdiv = $('<div id="zoomioloadingdiv"><div class="spinner"></div></div>').appendTo(document.body)
+		if (!ismobile){
+			$zoomiocontainer.on('mousemove', function(e){
+				var $zoomimage = currentzoominfo.$zoomimage
+				var imgoffset = currentzoominfo.offset
+				var mousecoord = [e.pageX-imgoffset.left, e.pageY-imgoffset.top]
+				var multiplier = currentzoominfo.multiplier
+				$zoomimage.css({left: -mousecoord[0] * multiplier[0] + mousecoord[0] , top: -mousecoord[1] * multiplier[1] + mousecoord[1]})
+			})
+			$zoomiocontainer.on('mouseleave', function(){
+				$zoomioloadingdiv.css({visibility: 'hidden'})
+				$zoomiocontainer.stop().animate({opacity:0}, currentzoominfo.settings.fadeduration, function(){
+					$(this).css({visibility:'hidden'})
+				})
+			})
+		}
+		else{ // is mobile
+			$zoomiocontainer.addClass('mobileclass')
+			$zoomiocontainer.on('touchstart', function(e){
+				e.stopPropagation() // stopPropagation() works on jquery evt object (versus e.originalEvent.changedTouches[0]
+			})
+			$(document).on('touchstart', function(e){
+				if (currentzoominfo.$zoomimage){ // if $zoomimage defined
+					$zoomiocontainer.stop().animate({opacity:0}, currentzoominfo.settings.fadeduration, function(){
+						$(this).css({visibility:'hidden'})
+					})
+				}
+			})
+		} // end else
+	})
+
+})(jQuery);
